db6029d0bc76a7b9f7129697ed81731d4895eceb
Will Trojak <wt247@tamu.edu>
2020-12-02 15:18:46 -0600

    added check for input to src kernel

--- pyfr/solvers/base/elements.py
+++ pyfr/solvers/base/elements.py
@@ -155,11 +155,17 @@ class BaseElements(object):
 
     @lazyprop
     def _ploc_in_src_exprs(self):
-        return any(re.search(r'\bploc\b', ex) for ex in self._src_exprs)
+        ploc_src_exprs = any(re.search(r'\bploc\b', ex) for ex in self._src_exprs)
+        ploc_src_macro = self.cfg.hasopt('solver-source', 'source')
+
+        return ploc_src_exprs or ploc_src_macro
 
     @lazyprop
     def _soln_in_src_exprs(self):
-        return any(re.search(r'\bu\b', ex) for ex in self._src_exprs)
+        soln_src_exprs = any(re.search(r'\bu\b', ex) for ex in self._src_exprs)
+        soln_src_macro = self.cfg.hasopt('solver-source', 'source')
+
+        return soln_src_exprs or soln_src_macro
 
     def set_backend(self, backend, nscalupts, nonce, intoff):
         self._be = backend
