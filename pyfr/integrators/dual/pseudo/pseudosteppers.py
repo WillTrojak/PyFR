0234f8e050ef1e9041c60b3c06e5de3b607e7a2e
Will Trojak <wt247@tamu.edu>
2021-01-22 09:27:40 -0600

    white space

--- pyfr/integrators/dual/pseudo/pseudosteppers.py
+++ pyfr/integrators/dual/pseudo/pseudosteppers.py
@@ -51,7 +51,6 @@ class BaseDualPseudoStepper(BaseDualPseudoIntegrator):
         axnpby = self._get_axnpby_kerns(2 + stpn + nstg, subdims=self._subdims)
         self._prepare_reg_banks(fout, self._idxcurr, *self._stepper_regidx,
                                 *self._stage_regidx[:nstg])
-
         self._queue.enqueue_and_run(axnpby, *self._stepper_coeffs)
 
 
