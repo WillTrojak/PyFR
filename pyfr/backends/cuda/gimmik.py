895ec8d49069b68c9456745b21964549cf10dab7
Will Trojak <wt247@tamu.edu>
2021-08-03 16:12:17 -0500

    Added warp splitting gimmik call.

--- pyfr/backends/cuda/gimmik.py
+++ pyfr/backends/cuda/gimmik.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-from gimmik import generate_mm
+from gimmik import generate_mm, generate_mm_split
 import numpy as np
 
 from pyfr.backends.base import ComputeKernel, NotSuitableError
@@ -29,19 +29,23 @@ class CUDAGiMMiKKernels(CUDAKernelProvider):
         if nuq > 28 and nnz / arr.size > 0.15:
             raise NotSuitableError('Matrix is inappropriate for GiMMiK')
 
+        order = self.backend.cfg.get('solver', 'order', 0)
+        split = order + 1
+
+        # Determine the grid/block
+        block_s = (32, 1, 1)
+        block = (32*split, 1, 1)
+        grid = get_grid_for_block(block_s, b.ncol)
+
         # Generate
-        src = generate_mm(arr, dtype=a.dtype, platform='cuda',
-                          alpha=alpha, beta=beta)
+        src = generate_mm_split(arr, dtype=a.dtype, platform='cuda',
+                                alpha=alpha, beta=beta, block_dim=block[0],
+                                split=split)
 
         # Build
         fun = self._build_kernel('gimmik_mm', src,
                                  [np.int32, np.intp]*2 + [np.int32])
         fun.set_cache_pref(prefer_l1=True)
-
-
-        # Determine the grid/block
-        block = (128, 1, 1)
-        grid = get_grid_for_block(block, b.ncol)
 
         class MulKernel(ComputeKernel):
             def run(self, queue):
