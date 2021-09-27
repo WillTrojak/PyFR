78b979e3a108713167a89aee2878341ba2725c84
Will Trojak <wt247@tamu.edu>
2021-09-27 11:15:21 -0500

    Added double precision tfmm gimmik support.

--- pyfr/backends/cuda/gimmik.py
+++ pyfr/backends/cuda/gimmik.py
@@ -54,6 +54,9 @@ class CUDAGiMMiKKernels(CUDAKernelProvider):
         return MulKernel()
 
     def mul_tensor(self, a, b, out):
+        # Ensure the matrices are compatible
+        print(f'{a.nrow=} {a.ncol=} {b.nrow=} {b.ncol} {out.nrow=} {out.ncol=}')
+
         # Check that A is constant
         if 'const' not in a.tags:
             raise NotSuitableError('GiMMiK requires a constant a matrix')
@@ -62,54 +65,94 @@ class CUDAGiMMiKKernels(CUDAKernelProvider):
         arr = a.get()
         nnz, nuq = np.count_nonzero(arr), len(np.unique(np.abs(arr)))
 
-        # Check that A is suitable
-        if nuq > 28 and nnz / arr.size > 0.15:
-            raise NotSuitableError('Matrix is inappropriate for GiMMiK')
-
         (p, k) = np.shape(arr)
         nele = int(b.ncol/13)
 
         shr_size = 0
 
         # Generate
-        if p == 5:
-            l1_pref  = False
-            shr_pref = True
-            src, shr_size = generate_tfmm_lines(arr, ndims=3, nvars=13,
-                                dtype=a.dtype, soasz=32, shr_max=96000,
-                                platform='cuda_tensor_lines_24')
-            block = (200, 1, 1)
-            grid = (ceil(nele*p*p/block[0]), 1, 1)
-        elif p == 4:
-            l1_pref  = False
-            shr_pref = True
-            opargs = {'ld_opt': True, 'st_opt': False, 'intl_opt': False,
-                      'pipe_opt': False, 'max_coag': 0, 'compute_size': 0,
-                      'shr_op_order': 'grs', 'shr_bdc': True}
-            src, shr_size = generate_tfmm_managed(arr, ndims=3, nvars=13, 
-                                soasz=32, dtype=a.dtype, opargs=opargs,
-                                platform='cuda_tfmm_managed', flux='hyperns',
-                                shared_max=67584, ufc_size=4*(2**15), 
-                                block_dim=128)
-            block = (128, 1, 1)
-            warp_size = 32
-            elem_warp = int(warp_size/p)
-            elem_block = int(block[0]/warp_size)*elem_warp
-            grid = (ceil(nele/elem_block), 1, 1)
-        elif p <= 3:
-            l1_pref  = False
-            shr_pref = True
+        if a.dtype == np.float32:
+            if p == 5:
+                l1_pref  = False
+                shr_pref = True
+                src, shr_size = generate_tfmm_lines(arr, ndims=3, nvars=13,
+                                    dtype=a.dtype, soasz=32, shr_max=96000,
+                                    platform='cuda_tensor_lines_24')
+                block = (200, 1, 1)
+                grid = (ceil(nele*p*p/block[0]), 1, 1)
+            elif p == 4:
+                l1_pref  = False
+                shr_pref = True
+                opargs = {'ld_opt': True, 'st_opt': False, 'intl_opt': False,
+                          'pipe_opt': False, 'max_coag': 0, 'compute_size': 0,
+                          'shr_op_order': 'grs', 'shr_bdc': True}
+                src, shr_size = generate_tfmm_managed(arr, ndims=3, nvars=13, 
+                                    soasz=32, dtype=a.dtype, opargs=opargs,
+                                    platform='cuda_tfmm_managed', flux='hyperns',
+                                    shared_max=67584, ufc_size=4*(2**15), 
+                                    block_dim=128)
+                block = (128, 1, 1)
+                warp_size = 32
+                elem_warp = int(warp_size/p)
+                elem_block = int(block[0]/warp_size)*elem_warp
+                grid = (ceil(nele/elem_block), 1, 1)
+            elif p <= 3:
+                l1_pref  = False
+                shr_pref = True
 
-            src, shr_size = generate_tfmm(arr, ndims=3, nvars=13, dtype=a.dtype,
-                                block_dim=128, soasz=32, flux='hyper',
-                                platform='cuda_tensor_flux12')
-            block = (128, 1, 1)
-            warp_size = 32
-            elem_warp = int(warp_size/p)
-            elem_block = int(block[0]/warp_size)*elem_warp
-            grid = (ceil(nele/elem_block), 1, 1)        
-
-        src = f'extern "C"\n{{\n{src}\n}}'
+                src, shr_size = generate_tfmm(arr, ndims=3, nvars=13, dtype=a.dtype,
+                                    block_dim=128, soasz=32, flux='hyper',
+                                    platform='cuda_tensor_flux12')
+                block = (128, 1, 1)
+                warp_size = 32
+                elem_warp = int(warp_size/p)
+                elem_block = int(block[0]/warp_size)*elem_warp
+                grid = (ceil(nele/elem_block), 1, 1)        
+        elif a.dtype == np.float64:
+            if p == 5:
+                l1_pref  = False
+                shr_pref = True
+                src, shr_size = generate_tfmm_lines(arr, ndims=3, nvars=13,
+                                    dtype=a.dtype, soasz=32, shr_max=96000,
+                                    platform='cuda_tensor_lines_12')
+                block = (200, 1, 1)
+                grid = (ceil(nele*p*p/block[0]), 1, 1)
+            if p == 4:
+                l1_pref  = False
+                shr_pref = True
+                src, shr_size = generate_tfmm_lines(arr, ndims=3, nvars=13,
+                                    dtype=a.dtype, soasz=32, shr_max=92160,
+                                    platform='cuda_tensor_lines_15')
+                block = (192, 1, 1)
+                grid = (ceil(nele*p*p/block[0]), 1, 1)
+            if p == 3:
+                l1_pref  = False
+                shr_pref = True
+                shr_max = 60416
+                opargs = {'ld_opt': True, 'st_opt': False, 'intl_opt': False,
+                        'pipe_opt': False, 'max_coag': 0, 'compute_size': 0,
+                        'shr_op_order': 'grs', 'shr_bdc': True}
+                src, shr_size = generate_tfmm_managed(arr, ndims=3, nvars=13, 
+                                    soasz=32, dtype=a.dtype, opargs=opargs,
+                                    platform='cuda_tfmm_managed', flux='hyperns',
+                                    shared_max=shr_max, ufc_size=4*(2**15), 
+                                    block_dim=128)
+                block = (128, 1, 1)
+                grid = (ceil(nele*p/block[0]), 1, 1)
+            if p == 2:
+                l1_pref  = False
+                shr_pref = True
+                shr_max = 43008
+                opargs = {'ld_opt': True, 'st_opt': False, 'intl_opt': False,
+                        'pipe_opt': False, 'max_coag': 0, 'compute_size': 0,
+                        'shr_op_order': 'grs', 'shr_bdc': True}
+                src, shr_size = generate_tfmm_managed(arr, ndims=3, nvars=13, 
+                                    soasz=32, dtype=a.dtype, opargs=opargs,
+                                    platform='cuda_tfmm_managed', flux='hyperns',
+                                    shared_max=shr_max, ufc_size=4*(2**15), 
+                                    block_dim=128)
+                block = (128, 1, 1)
+                grid = (ceil(nele*p/block[0]), 1, 1)
 
         print(f'{nele=}, {shr_size=}, {p=}, {block=}, {grid=}')
 
@@ -119,20 +162,6 @@ class CUDAGiMMiKKernels(CUDAKernelProvider):
         f = open(src_name, 'w')
         f.write(src)
         f.close()
-
-        # sm = 70
-        # comp = 'nvcc'
-        #opt = (f'--gpu-code=compute_{sm} --gpu-architecture=compute_{sm}'
-        #        ' -O3 --std=c++17 --use_fast_math --fassociative-math')
-        #comp_cmd = f'{comp} {src_name} {opt} -ptx -o {ptx_name}'
-        # opt = (f"-arch='sm_{sm}' -O3 --std=c++17 --use_fast_math --fassociative-math")
-        # comp_cmd = f'{comp} {src_name} {opt} -cubin -o {ptx_name}'
-        # print(comp_cmd)
-        # nvcc_out = (subprocess.check_output(comp_cmd, stderr=subprocess.STDOUT, shell=True)).decode("utf-8")
-
-        # f = open(ptx_name, 'rb')
-        # ptx = f.read()
-        # f.close()
 
         # Build
         fun = self._build_kernel('gimmik_tfmm', src,
@@ -148,6 +177,7 @@ class CUDAGiMMiKKernels(CUDAKernelProvider):
 
         return MulKernel()
 
+
     def mul_tensor_source(self, a, b, out):
         # Check that A is constant
         if 'const' not in a.tags:
@@ -157,52 +187,96 @@ class CUDAGiMMiKKernels(CUDAKernelProvider):
         arr = a.get()
         nnz, nuq = np.count_nonzero(arr), len(np.unique(np.abs(arr)))
 
-        # Check that A is suitable
-        if nuq > 28 and nnz / arr.size > 0.15:
-            raise NotSuitableError('Matrix is inappropriate for GiMMiK')
-
         (p, k) = np.shape(arr)
         nele = int(b.ncol/13)
 
         shr_size = 0
 
         # Generate
-        if p == 5:
-            l1_pref  = False
-            shr_pref = True
-            src, shr_size = generate_tfmm_lines(arr, ndims=3, nvars=13,
-                                dtype=a.dtype, soasz=32, shr_max=96000,
-                                platform='cuda_S_tensor_lines_24')
-            block = (200, 1, 1)
-            grid = (ceil(nele*p*p/block[0]), 1, 1)
-        elif p == 4:
-            l1_pref  = False
-            shr_pref = True
-            opargs = {'ld_opt': True, 'st_opt': False, 'intl_opt': False,
-                      'pipe_opt': False, 'max_coag': 0, 'compute_size': 0,
-                      'shr_op_order': 'grs', 'shr_bdc': True}
-            src, shr_size = generate_tfmm_managed(arr, ndims=3, nvars=13, 
-                                soasz=32, dtype=a.dtype, opargs=opargs,
-                                platform='cuda_tfmm_managed', flux='hyperns',
-                                shared_max=67584, ufc_size=4*(2**15), 
-                                block_dim=128, source_term=True)
-            block = (128, 1, 1)
-            warp_size = 32
-            elem_warp = int(warp_size/p)
-            elem_block = int(block[0]/warp_size)*elem_warp
-            grid = (ceil(nele/elem_block), 1, 1)
-        elif p <= 3:
-            l1_pref  = False
-            shr_pref = True
+        if a.dtype == np.float32:
+            if p == 5:
+                l1_pref  = False
+                shr_pref = True
+                src, shr_size = generate_tfmm_lines(arr, ndims=3, nvars=13,
+                                    dtype=a.dtype, soasz=32, shr_max=96000,
+                                    platform='cuda_S_tensor_lines_24')
+                block = (200, 1, 1)
+                grid = (ceil(nele*p*p/block[0]), 1, 1)
+            elif p == 4:
+                l1_pref  = False
+                shr_pref = True
+                opargs = {'ld_opt': True, 'st_opt': False, 'intl_opt': False,
+                        'pipe_opt': False, 'max_coag': 0, 'compute_size': 0,
+                        'shr_op_order': 'grs', 'shr_bdc': True}
+                src, shr_size = generate_tfmm_managed(arr, ndims=3, nvars=13, 
+                                    soasz=32, dtype=a.dtype, opargs=opargs,
+                                    platform='cuda_tfmm_managed', flux='hyperns',
+                                    shared_max=67584, ufc_size=4*(2**15), 
+                                    block_dim=128, source_term=True)
+                block = (128, 1, 1)
+                warp_size = 32
+                elem_warp = int(warp_size/p)
+                elem_block = int(block[0]/warp_size)*elem_warp
+                grid = (ceil(nele/elem_block), 1, 1)
+            elif p <= 3:
+                l1_pref  = False
+                shr_pref = True
 
-            src, shr_size = generate_tfmm(arr, ndims=3, nvars=13, dtype=a.dtype,
-                                block_dim=128, soasz=32, flux='hyper',
-                                platform='cuda_S_tensor_flux12')
-            block = (128, 1, 1)
-            warp_size = 32
-            elem_warp = int(warp_size/p)
-            elem_block = int(block[0]/warp_size)*elem_warp
-            grid = (ceil(nele/elem_block), 1, 1)
+                src, shr_size = generate_tfmm(arr, ndims=3, nvars=13, dtype=a.dtype,
+                                    block_dim=128, soasz=32, flux='hyper',
+                                    platform='cuda_S_tensor_flux12')
+                block = (128, 1, 1)
+                warp_size = 32
+                elem_warp = int(warp_size/p)
+                elem_block = int(block[0]/warp_size)*elem_warp
+                grid = (ceil(nele/elem_block), 1, 1)
+        if a.dtype == np.float64:
+            if p == 5:
+                l1_pref  = False
+                shr_pref = True
+                src, shr_size = generate_tfmm_lines(arr, ndims=3, nvars=13,
+                                    dtype=a.dtype, soasz=32, shr_max=96000,
+                                    platform='cuda_S_tensor_lines_12')
+                block = (200, 1, 1)
+                grid = (ceil(nele*p*p/block[0]), 1, 1)
+            if p == 4:
+                l1_pref  = False
+                shr_pref = True
+
+                src, shr_size = generate_tfmm_lines(arr, ndims=3, nvars=13,
+                                    dtype=a.dtype, soasz=32, shr_max=92190,
+                                    platform='cuda_S_tensor_lines_15')
+                block = (192, 1, 1)
+                grid = (ceil(nele*p*p/block[0]), 1, 1)
+            if p == 3:
+                l1_pref  = False
+                shr_pref = True
+                shr_max = 60416
+                opargs = {'ld_opt': True, 'st_opt': False, 'intl_opt': False,
+                        'pipe_opt': False, 'max_coag': 0, 'compute_size': 0,
+                        'shr_op_order': 'grs', 'shr_bdc': True}
+                
+                src, shr_size = generate_tfmm_managed(arr, ndims=3, nvars=13, 
+                                    soasz=32, dtype=a.dtype, opargs=opargs,
+                                    platform='cuda_tfmm_managed', flux='hyperns',
+                                    shared_max=shr_max, ufc_size=4*(2**15), 
+                                    block_dim=128, source_term=True)
+                block = (128, 1, 1)
+                grid = (ceil(nele*p/block[0]), 1, 1)
+            if p == 2:
+                l1_pref  = False
+                shr_pref = True
+                shr_max = 43008
+                opargs = {'ld_opt': True, 'st_opt': False, 'intl_opt': False,
+                        'pipe_opt': False, 'max_coag': 0, 'compute_size': 0,
+                        'shr_op_order': 'grs', 'shr_bdc': True}
+                src, shr_size = generate_tfmm_managed(arr, ndims=3, nvars=13, 
+                                    soasz=32, dtype=a.dtype, opargs=opargs,
+                                    platform='cuda_tfmm_managed', flux='hyperns',
+                                    shared_max=shr_max, ufc_size=4*(2**15), 
+                                    block_dim=128, source_term=True)
+                block = (128, 1, 1)
+                grid = (ceil(nele*p/block[0]), 1, 1)
 
         print(f'{nele=}, {shr_size=}, {p=}, {block=}, {grid=}')
 
--- pyfr/shapes.py
+++ pyfr/shapes.py
@@ -156,7 +156,7 @@ class BaseShape(object):
 
     @lazyprop
     def m99(self):
-        nx = 32
+        nx = 26
         lx = 2*np.pi
         smats = pow(lx/(2*nx), 2)
         m_rcpdjac = ((nx/np.pi)**3)
