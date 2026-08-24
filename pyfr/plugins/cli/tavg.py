553dbb4f5cea7ae01cc21719e2fcf711b2880c0c
Freddie Witherden <freddie@witherden.org>
2026-08-24 09:27:03 -0500

    Allow tavg merge to run under MPI.

--- pyfr/plugins/cli/tavg.py
+++ pyfr/plugins/cli/tavg.py
@@ -1,3 +1,4 @@
+import os
 import re
 
 import h5py
@@ -5,13 +6,14 @@ import numpy as np
 from numpy.lib.recfunctions import structured_to_unstructured as s2u
 
 from pyfr.inifile import Inifile
+from pyfr.mpiutil import get_comm_rank_root, init_mpi, mpi
 from pyfr.plugins.base import BaseCLIPlugin
 from pyfr.plugins.common import cli_external, get_elementscls
-from pyfr.progress import NullProgressBar
+from pyfr.progress import NullProgressBar, NullProgressSequence
 from pyfr.shapes import BaseShape
 from pyfr.stats import eval_algebraic, tavg_exprs
 from pyfr.stats.provider import name_tokens
-from pyfr.util import first, merge_intervals, subclass_where
+from pyfr.util import first, merge_intervals, pwrite_all, subclass_where
 
 
 class TavgCLIPlugin(BaseCLIPlugin):
@@ -31,26 +33,31 @@ class TavgCLIPlugin(BaseCLIPlugin):
 
     @cli_external
     def merge_cli(self, args):
+        # Initialise MPI
+        init_mpi()
+
+        # Get our MPI info
+        comm, rank, root = get_comm_rank_root()
+
         self.report = args.report
 
+        # Only the root rank reports progress
+        progress = args.progress if rank == root else NullProgressSequence()
+
         # Open all the solution files
-        with args.progress.start('Preprocess files'):
+        with progress.start('Preprocess files'):
             self._preprocess_files(args.solns)
 
             # Initialise things needed for the merge
             self._init_tavg_merge()
 
-        with h5py.File(args.output, 'w', libver='latest') as outf:
-            with args.progress.start('Prepare output file'):
-                self._prepare_output_file(outf)
+        # Lay down the output file and obtain the dataset offsets
+        with progress.start('Prepare output file'):
+            doffs = self._prepare_output_file(args.output)
 
-            # Merge the averages
-            with args.progress.start_with_bar('Merge data') as pbar:
-                self._merge_data(outf, pbar)
-
-            # Merge the metadata
-            with args.progress.start('Merge metadata'):
-                self._merge_stats(outf)
+        # Merge the averages
+        with progress.start_with_bar('Merge data') as pbar:
+            self._merge_data(args.output, doffs, pbar)
 
         if self.report:
             self._report_convergence()
@@ -135,30 +142,59 @@ class TavgCLIPlugin(BaseCLIPlugin):
                             if n in te0.alg and evaluable(e)}
             self._rep = {g: np.zeros((2, len(ns))) for g, ns in
                          [('avg', self.anames), ('derived', self._alg)] if ns}
+            self._w2 = sum(w*w for *_, w in self.files)
 
-        # Break each dataset into ~2 GiB chunks
+        # Output record dtypes
+        self._odtypes = {f'tavg/{k}': self._odtype(dt)
+                         for k, dt in self._dtypes.items()}
+
+        comm, rank, root = get_comm_rank_root()
+
+        # Break our portion of each dataset into ~2 GiB chunks
         chunk_sz = -(2*1024**3 // -dtype0.itemsize)
-        self.chunks = [(k, slice(i, i + chunk_sz))
-                       for k, v in dshapes.items()
-                       for i in range(0, v[0], chunk_sz)]
 
-    def _prepare_output_file(self, outf):
-        basef = self.files[0][0]
+        self.chunks = []
+        for k, v in dshapes.items():
+            sidx = (rank*v[0]) // comm.size
+            eidx = ((rank + 1)*v[0]) // comm.size
 
-        # Copy over top level records except stats and tavg
-        for k, v in basef.items():
-            if k not in ('stats', 'tavg'):
-                basef.copy(v, outf, k)
+            for i in range(sidx, eidx, chunk_sz):
+                self.chunks.append((k, slice(i, min(i + chunk_sz, eidx))))
 
-        # Handle the tavg group
-        for k, v in basef['tavg'].items():
-            if re.match(r'p\d+-\w+-', k):
-                basef.copy(v, outf, f'tavg/{k}')
-            else:
-                dt = self._odtype(v.dtype)
-                w = outf.create_dataset(f'tavg/{k}', v.shape, dt)
-                for ak, av in v.attrs.items():
-                    w.attrs[ak] = av
+    def _prepare_output_file(self, path):
+        comm, rank, root = get_comm_rank_root()
+
+        # Have the root rank lay down the structure of the file
+        if rank == root:
+            basef, doffs = self.files[0][0], {}
+
+            with h5py.File(path, 'w', libver='latest') as outf:
+                # Copy over top level records except stats and tavg
+                for k, v in basef.items():
+                    if k not in ('stats', 'tavg'):
+                        basef.copy(v, outf, k)
+
+                # Handle the tavg group
+                for k, v in basef['tavg'].items():
+                    if re.match(r'p\d+-\w+-', k):
+                        basef.copy(v, outf, f'tavg/{k}')
+                    else:
+                        dt = self._odtypes[f'tavg/{k}']
+                        w = outf.create_dataset(f'tavg/{k}', v.shape, dt)
+                        for ak, av in v.attrs.items():
+                            w.attrs[ak] = av
+
+                # Merge the metadata
+                self._merge_stats(outf)
+
+                # Allocate the average datasets and note their offsets
+                for k in self._odtypes:
+                    outf[k][(-1,)*outf[k].ndim] = 0
+                    doffs[k] = outf[k].id.get_offset()
+
+            return comm.bcast(doffs, root=root)
+        else:
+            return comm.bcast(None, root=root)
 
     def _odtype(self, idtype):
         sdt = idtype['avg'][self.anames[0]]
@@ -169,61 +205,110 @@ class TavgCLIPlugin(BaseCLIPlugin):
         dg = data[group][self.anames]
         return s2u(dg).reshape(len(data), len(dg.dtype), -1)
 
-    def _merge_data(self, outf, pbar=NullProgressBar()):
-        file0 = self.files[0][0]
+    def _merge_data(self, path, doffs, pbar=NullProgressBar()):
+        comm, rank, root = get_comm_rank_root()
 
+        # Open the output file for raw writing
+        fd = os.open(path, os.O_WRONLY)
+
+        try:
+            self._merge_chunks(fd, doffs, pbar)
+        finally:
+            os.close(fd)
+
+        # Ensure all ranks have finished writing
+        comm.barrier()
+
+    def _merge_chunks(self, fd, doffs, pbar):
         for k, s in pbar.start_with_iter(self.chunks):
-            # Initialise the accumulator
-            shape = self._unpack(file0[k][s], 'avg').shape
-            acc = np.zeros(shape, dtype=float)
+            acc, t = None, 0
 
             # Merge the base averages
-            t = 0
             for file, *_, w in self.files:
-                acc += w*self._unpack(file[k][s], 'avg')
+                d = self._unpack(file[k][s], 'avg')
+
+                # Initialise the accumulator
+                if acc is None:
+                    acc = np.zeros(d.shape)
+                    if self.report:
+                        self._init_report_chunk(d)
+
+                acc += w*d
                 t += w
 
-            # Accumulate window-to-window convergence statistics
+                # Accumulate window-to-window convergence statistics
+                if self.report:
+                    self._accumulate_report(d, w)
+
+            # Reduce the statistics against the merged mean
             if self.report:
-                self._accumulate_report(k, s, acc / t)
+                self._reduce_report(acc / t)
 
             # Build output record
-            out = np.empty(acc.shape[0], dtype=outf[k].dtype)
+            out = np.empty(acc.shape[0], dtype=self._odtypes[k])
 
             # Pack merged averages
             for fn, col in zip(self.anames, acc.swapaxes(0, 1)):
                 out['avg'][fn] = col
 
-            outf[k][s] = out
+            pwrite_all(fd, doffs[k] + s.start*out.itemsize, out)
 
-    def _accumulate_report(self, k, s, m):
-        rep = self._rep
+    def _init_report_chunk(self, d0):
+        # Shift the deviation sums by the first window of the chunk
+        self._ref = ref = d0.astype(float)
+        self._dev = {'avg': np.zeros((2, *ref.shape))}
+
+        if 'derived' in self._rep:
+            self._fref = self._eval_derived(ref.swapaxes(0, 1))
+            self._dev['derived'] = np.zeros((2, *self._fref.shape))
+
+    def _accumulate_report(self, d, w):
+        dev, ww, e = self._dev, w*w, d - self._ref
+
+        # Weighted first and second moments of the shifted deviations
+        dev['avg'][0] += ww*e
+        dev['avg'][1] += ww*e*e
+
+        if 'derived' in dev:
+            g = self._eval_derived((self._ref + e).swapaxes(0, 1))
+            g -= self._fref
+
+            dev['derived'][0] += ww*g
+            dev['derived'][1] += ww*g*g
+
+    def _reduce_report(self, m):
+        rep, c = self._rep, self._w2
+        mu, (e1, e2) = m - self._ref, self._dev['avg']
+
+        # Weighted squared deviation of each window from the merged mean
+        rep['avg'][0] += (e2 - 2*mu*e1 + c*mu*mu).sum(axis=(0, 2))
         rep['avg'][1] += (m*m).sum(axis=(0, 2))
 
         if 'derived' in rep:
             mf = self._eval_derived(m.swapaxes(0, 1))
+            nu, (g1, g2) = mf - self._fref, self._dev['derived']
+
+            rep['derived'][0] += (g2 - 2*nu*g1 + c*nu*nu).sum(axis=(0, 2))
             rep['derived'][1] += (mf*mf).sum(axis=(0, 2))
 
-        # Weighted squared deviation of each window from the merged mean
-        for file, *_, w in self.files:
-            dev = self._unpack(file[k][s], 'avg') - m
-            rep['avg'][0] += w*w*(dev*dev).sum(axis=(0, 2))
-
-            if 'derived' in rep:
-                df = self._eval_derived((dev + m).swapaxes(0, 1)) - mf
-                rep['derived'][0] += w*w*(df*df).sum(axis=(0, 2))
-
     def _report_convergence(self):
+        comm, rank, root = get_comm_rank_root()
+
+        # Reduce the statistics accumulated by each rank
+        for v in self._rep.values():
+            comm.Allreduce(mpi.IN_PLACE, v, op=mpi.SUM)
+
         # Batch-means small sample correction factor
         n = len(self.files)
-        fac = 1/(1 - sum(w*w for *_, w in self.files))
+        fac = 1/(1 - self._w2)
 
-        print(f'Relative standard error over {n} windows')
-        gnames = {'avg': self.anames, 'derived': self._alg}
-        for g, (se2, m2) in self._rep.items():
-            rse = np.sqrt(fac*se2)/np.maximum(np.sqrt(m2), 1e-300)
-            for name, r in zip(gnames[g], rse):
-                print(f'{g}-{name}\t{r:.4e}')
+        if rank == root:
+            print(f'Relative standard error over {n} windows')
+            gnames = {'avg': self.anames, 'derived': self._alg}
+            for g, (se2, m2) in self._rep.items():
+                rse = np.sqrt(fac*se2)/np.maximum(np.sqrt(m2), 1e-300)
+                for name, r in zip(gnames[g], rse):
+                    print(f'{g}-{name}\t{r:.4e}')
 
     def _merge_stats(self, outf):
         nstats = Inifile()
